9bd0d8356024ee5ca8a697c6342ad597ff79f507
nickwai1 <53497786+nickwai1@users.noreply.github.com>
2021-09-28 11:12:25 -0700

    postgres schema done need review

--- database-mongo/index.js
+++ database-mongo/index.js
@@ -1,5 +1,16 @@
-const mongoose = require('mongoose');
+// const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost:27017/qa', {useNewUrlParser: true, useUnifiedTechnology: true});
+const { Mongoose } = require("mongoose");
 
-const db = mongoose.connection;
+// mongoose.connect('mongodb://localhost:27017/qa', {useNewUrlParser: true, useUnifiedTechnology: true});
+
+// const db = mongoose.connection;
+
+// db.on('error', console.error.bind(console, 'connection error:'));
+// db.once('open', function() {
+//     console.log('Connected!')
+// });
+
+const qa = new Mongoose.schema({
+    
+})
--- schema.sql
+++ schema.sql
@@ -1,5 +1,37 @@
 CREATE DATABASE QA;
 
+DROP TABLE IF EXISTS questions;
+
 CREATE TABLE questions(
-    
+    question_id INT AUTO_INCREMENT,
+    question_body VARCHAR(200),
+    question_date DATE NOT NULL,
+    asker_name VARCHAR(15),
+    question_helpfulness INT,
+    reported BOOLEAN NOT NULL,
+    answer_id INT,
+    product_id INT,
+    PRIMARY KEY (question_id),
+    FOREIGN KEY (answer_id) REFERENCES answers(answer_id),
+    FOREIGN KEY (product_id) REFERENCES products(product_id)
+)
+
+DROP TABLE IF EXISTS answers;
+
+CREATE TABLE answers(
+    a_id INT AUTO_INCREMENT,
+    body VARCHAR(200),
+    answerer_name VARCHAR(20),
+    helpfulness INT,
+    reported BOOLEAN NOT NULL,
+    photo_id INT,
+    PRIMARY KEY (a_id),
+    FOREIGN KEY (photo_id) REFERENCES photos(photo_id)
+)
+
+DROP TABLE IF EXISTS photos;
+
+CREATE TABLE photos(
+    p_id INT AUTO_INCREMENT,
+    url VARCHAR(100)
 )
