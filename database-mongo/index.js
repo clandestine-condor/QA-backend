77a95ab6dda1fa30d1695ff06324979d88f55722
nickwai1 <53497786+nickwai1@users.noreply.github.com>
2021-09-29 13:38:13 -0700

    clean up mongoose model

--- database-mongo/index.js
+++ database-mongo/index.js
@@ -1,22 +1,15 @@
-// const mongoose = require('mongoose');
+const mongoose = require('mongoose');
 
 const { Mongoose } = require("mongoose");
 
-// mongoose.connect('mongodb://localhost:27017/qa', {useNewUrlParser: true, useUnifiedTechnology: true});
+mongoose.connect('mongodb://localhost:27017/qa', {useNewUrlParser: true, useUnifiedTechnology: true});
 
-// const db = mongoose.connection;
+const db = mongoose.connection;
 
-// db.on('error', console.error.bind(console, 'connection error:'));
-// db.once('open', function() {
-//     console.log('Connected!')
-// });
-// const photos = mongoose.model(
-//     "photos",
-//     new mongoose.Schema({
-//         pId: Number,
-//         url: String
-//     })
-// );
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+    console.log('Connected!')
+});
 
 const answers = mongoose.model(
     "answers",
