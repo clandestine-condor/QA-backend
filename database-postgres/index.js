76fe919ecd891d77b486d5342620cfb489340213
nickwai1 <53497786+nickwai1@users.noreply.github.com>
2021-09-30 16:23:48 -0700

    completed 2 queries

--- database-postgres/index.js
+++ database-postgres/index.js
@@ -1,15 +1,11 @@
 const { Pool } = require('pg');
+
 const pool = new Pool({
     user: 'nick',
     host: 'localhost',
     database: 'postgres',
     password: '',
     post: 3211,
-})
-
-pool.query('SELECT $1::text as message', ['Hello from DB!'], (err, res) => {
-    console.log(err ? err.stack : res.rows[0].message) // Hello World!
-    pool.end()
-})
+});
 
 module.exports = pool;
--- server/index.js
+++ server/index.js
@@ -4,10 +4,29 @@ const PORT = 3000;
 const db = require('../database-postgres')
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
+app.get('/qa/questions/:id', (req, res) => {
+    const {id} = req.params;
+    const query = 'SELECT * FROM questions WHERE product_id = $1';
+    db.query(query, [id], (err, results) => {
+        if(err) {
+            res.status(404).send(err);
+        } else {
+            res.send(results.rows);
+        }
+    });
+});
+
+app.get('/qa/answers/:id/answers', (req, res) => {
+    const {id} = req.params;
+    const query = 'SELECT * FROM answers WHERE question_id = $1';
+    db.query(query, [id], (err, results) => {
+        if(err) {
+            res.status(404).send(err);
+        } else {
+            res.send(results.rows);
+        }
+    });
 }); 
 
 app.listen(PORT, () => {
